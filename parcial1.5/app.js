f2c454f143a51b4a054a049b224e7d1d1e2f7144
b1t3 <69220436+YuliCenteno@users.noreply.github.com>
2025-05-09 08:51:44 -0300

    Parcial1.5

---
+++ parcial1.5/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+(async function start() {
+    // Validar conexión a la base de datos.
+
+    // Iniciar el servidor
+    app.listen(PORT, () => {
+        console.log(`Servidor iniciado y escuchando en el puerto ${PORT}`);
+    });
+})();
